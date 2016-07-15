89027c528c2fa9f08ccf5ad20fe57d822a520cc0
Matthew Giem <matthew.giem@gmail.com>
2016-07-15 09:24:54 -0700

    create some basic erb files and the app.rb paths to connect them

--- app.rb
+++ app.rb
@@ -10,3 +10,23 @@ DB = PG.connect({:dbname => "hair_salon"})
 get('/') do
   erb(:index)
 end
+
+get('/owner') do
+  erb(:owner)
+end
+
+get('/owner/clients') do
+  erb(:clients)
+end
+
+get('/owner/clients/new') do
+  erb(:client_form)
+end
+
+get('/owner/stylists') do
+  erb(:stylists)
+end
+
+get('/owner/stylists/new') do
+  erb(:stylists_form)
+end
